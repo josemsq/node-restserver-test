6a1069af0c169f833052c8548d92c92aa91487e2
josemsq <josemsq@gmail.com>
2020-06-28 22:36:04 -0500

    1er commit rest server

---
+++ server/server.js
@@ -0,0 +1,53 @@
+require('./config/config');
+
+const express = require('express');
+const bodyParser = require('body-parser')
+const app = express();
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+    res.json('Hola mundo');
+});
+
+app.get('/usuario', (req, res) => {
+    res.json('get usuario');
+});
+
+app.post('/usuario', (req, res) => {
+
+    let body = req.body; //bodyParser
+
+    if (body.nombre === undefined) {
+        res.status(400).json({
+            ok: false,
+            mensaje: "El nombre es necesario"
+        });
+    } else {
+        res.json({ persona: body });
+    }
+
+});
+
+app.put('/usuario/:id', (req, res) => {
+
+    let id = req.params.id;
+    res.json({
+        id
+    });
+});
+
+app.delete('/usuario', (req, res) => {
+    res.json('delete usuario');
+});
+
+
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`Escuchando peticiones en el puerto`, 3000);
+});
