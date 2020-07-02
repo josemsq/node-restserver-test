1862b2284cfc57ebed88adec737b7c4d5a86aac0
Jose <josemsq@gmail.com>
2020-07-02 01:36:48 -0500

    RestServer - fix caducidad token 24h

--- server/config/config.js
+++ server/config/config.js
@@ -18,7 +18,7 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 // 30 dias
 // ===========
 
-process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30;
+process.env.CADUCIDAD_TOKEN = '24h';
 
 // ===========
 // Base de datos
